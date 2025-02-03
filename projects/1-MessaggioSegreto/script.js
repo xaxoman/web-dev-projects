289860baec25c6b980b9d744f80cf378323e4784
xaxoman <dennisx.fjdv8@silomails.com>
2025-02-03 14:34:27 +0100

    aggiunto i progetti node

--- projects/1-MessaggioSegreto/script.js
+++
@@ -1,5 +0,0 @@
-function show() {
-
-    // funzione chiamata tramite il click del bottone che cambia il testo dell'elemento "secret"
-    document.getElementById("secret").innerHTML = "not telling you!";
-}
--- projects/6-UserInput/script.js
+++
@@ -1,7 +0,0 @@
-function register() {
-
-    const paragraph = document.querySelector("p");
-    // aggiungiamo la funzione "value" in fondo per poter usare questo dato più tardi
-        const username = document.getElementById("usernameInput").value; 
-        paragraph.innerHTML = username + " ti sei registrato correttamente!";                                  
-}
