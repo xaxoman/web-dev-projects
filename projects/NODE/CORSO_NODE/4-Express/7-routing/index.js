daf9fe9cf9d1634c09d9e947d835f7a4b9dbc4b1
xaxoman <dennisx.fjdv8@silomails.com>
2025-02-17 17:35:23 +0100

    testing .gitignore config

--- projects/NODE/CORSO_NODE/4-Express/7-routing/index.js
+++ projects/NODE/CORSO_NODE/4-Express/7-routing/index.js
@@ -6,7 +6,7 @@ const routers_persone = require('./routes/route_persone');
 // Middleware per leggere i dati in formato JSON
 app.use(express.json()); // VA SEMPRE MESSO PRIMA DEI ROUTES
 
-// Middleware per le route delle persone
+// Middleware per le route delle persone */
 app.use(routers_persone); 
 
 app.listen(port, () => {
